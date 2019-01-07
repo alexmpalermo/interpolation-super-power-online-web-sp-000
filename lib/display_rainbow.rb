a78a4c62354696ed826bf9b761f4a9cf952de41c
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-07 18:02:07 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 display_rainbow ['red','orange','yellow','green','blue','indigo','violet']
 
 def #display_rainbow(display_rainbow)
-puts "R: #{display_rainbow[0]}, O: #{display_rainbow[1]}, "
+puts "R: #{display_rainbow[0]}, O: #{display_rainbow[1]}, Y: #{display_rainbow[2]}, "
 end
